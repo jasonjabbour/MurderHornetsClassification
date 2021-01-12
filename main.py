f47030b47baa091296281eddd1d13eb0a8d1cc58
Jason Jabbour <jjj4se@virginia.edu>
2021-01-11 22:03:25 -0500

    start data

---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+
+#get directory path
+dir_path = os.path.dirname(os.path.realpath(__file__))
+dir_path = dir_path.replace('\\','/')
+
+#load images 
+
+
+
+#driver
+if __name__ == "__main__":
+    pass
